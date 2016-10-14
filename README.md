dd9e668b588da038c546c29cb45ac03cbed7dbb3
soxface <sonyacorcoran@gmail.com>
2016-10-14 15:08:18 +1100

    added form with toast

---
+++ README.md
@@ -0,0 +1,12 @@
+# README
+
+## Deploy to Heroku
+
+- 'heroku login'
+- 'git init'
+- 'heroku create journoethics --stack cedar --region us --buildpack https://github.com/AdmitHub/meteor-buildpack-horse.git'
+- 'heroku git:remote -a journoethics'
+- 'git add '
+- 'git add .'
+- 'git commit -m "First Deploy"'
+- 'git push heroku master'
---
+++ client/helper.js
@@ -0,0 +1,20 @@
+Template.table.helpers({
+  questions: [
+    { text: 'A journalist’s role is to investigate and report on everything that might interest readers. There are no subjects that are off limits.' },
+    { text: 'It is OK to report on the private life of anyone, as long as it’s accurate and interesting.' },
+    { text: 'It’s OK to report on the private life of anyone who has ever sought publicity.' },
+    { text: 'It’s OK to report on the private life of an elected politician.' },
+    { text: 'It’s OK to use anything you learn about someone via social media.' },
+    { text: 'It’s OK to use any information and pics you have obtained from Facebook.' },
+    { text: 'It’s OK for a journalist to pretend to be someone else if it’s the only way to get interesting quotes.' },
+    { text: 'It’s OK for a journalist to pretend to be someone else if that helps to expose a lawbreaker.' },
+    { text: 'It’s OK for a journalist to break the law if it exposes behaviour by a public figure that most readers would consider scandalous.' },
+    { text: 'It’s OK to pay for information if that is the only way to get the story.' },
+    { text: 'It’s OK to pay for on-the-record quotes, it that makes the story more interesting.' },
+    { text: 'It’s OK to improve quotes as long as you retain the spirit of what the person was trying to say.' },
+    { text: 'It’s OK to improve quotes as long as you check with the person quoted.' },
+    { text: 'It’s OK to interview a child if they are the only person who can help.' },
+    { text: 'It’s OK to break a confidence/ use an off-the-record remark if the person you are quoting has engaged in bad behaviour.' },
+    { text: 'It’s OK to accept a gift (eg air fare/ accommodation/ meal) from a person or company you are reporting on, as long as you remain impartial in what you write/ or as long as you disclose the gift in a footnote.' }
+  ]
+});
